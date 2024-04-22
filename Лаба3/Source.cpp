4438e60f694efa62331d600fdf0ae0a2595f199f
Polina <bychenkova.anya@gmail.com>
2024-04-22 15:03:12 +0300

    a1

--- Лаба3/Source.cpp
+++ Лаба3/Source.cpp
@@ -2,30 +2,30 @@
 #include <math.h>
 
 int main() {
-	int a, b, c;
+	int a1, b, c;
 	printf("Enter  2 numbers\n");
-	scanf_s("%d%d", &a, &b);
-	if (a >= 0 && b >= 0) { 
-		printf("|%d|+|%d| = ", a, b);
-		c = a + b;
+	scanf_s("%d%d", &a1, &b);
+	if (a1 >= 0 && b >= 0) { 
+		printf("|%d|+|%d| = ", a1, b);
+		c = a1 + b;
 		printf("%d", c);
 	}
 	else
-		if (a > 0 && b < 0) {
-			printf("|%d|+|%d| = ", a, b);
-			c = a - b;
+		if (a1 > 0 && b < 0) {
+			printf("|%d|+|%d| = ", a1, b);
+			c = a1 - b;
 			printf("%d", c);
 }
 	else
-			if (a < 0 && b > 0) {
-				printf("|%d|+|%d| = ", a, b);
-				c = -a + b;
+			if (a1 < 0 && b > 0) {
+				printf("|%d|+|%d| = ", a1, b);
+				c = -a1 + b;
 				printf("%d", c);
 			}
 			else
-				if (a < 0 && b < 0) {
-					printf("|%d|+|%d| = ", a, b);
-					c = -a - b;
+				if (a1 < 0 && b < 0) {
+					printf("|%d|+|%d| = ", a1, b);
+					c = -a1 - b;
 					printf("%d", c);
 				}
 				else
